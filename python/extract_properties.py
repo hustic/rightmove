5bb27f17ea99f2ede49307aefdc815e782d63715
zeynepbicer <mericzeynepbicer@gmail.com>
2023-03-25 18:09:36 +0000

    Load URL to database

--- python/extract_properties.py
+++ python/extract_properties.py
@@ -1,3 +1,4 @@
+from typing import TypedDict, List
 from sayn import task
 from sayn.tasks.task import Task
 from sayn.database import Database
@@ -8,24 +9,27 @@ import httpx
 BASE_URL = "https://www.rightmove.co.uk/"
 
 
+class propertyURL(TypedDict):
+    property_id: str
+    property_url: str
+
 
 @task(outputs="raw.property_links")
 def extract_property_links(context: Task, warehouse: Database):
-
     params = {
         "searchType": "RENT",
         "locationIdentifier": "REGION^1244",
-        "maxPrice" : 2000 ,
-        "minBedrooms" : 2,
-        "propertyTypes" : "flats",
+        "maxPrice": 2000,
+        "minBedrooms": 2,
+        "propertyTypes": "flats",
         # "primaryDisplayPropertyType" : "flats"
         # "sortType" : ,
         "includeLetAgreed": "false",
-        "index": 0
+        "index": 0,
     }
 
     with context.step("Get Propery Links"):
-        property_links = []
+        property_links: List[propertyURL] = []
 
         with httpx.Client(base_url=BASE_URL) as client:
             page = True
@@ -36,17 +40,15 @@ def extract_property_links(context: Task, warehouse: Database):
                 links = soup.select(".propertyCard-link")
 
                 for link in links:
-                    if (href := link['href']) != "":
-                        property_links.append(href)
+                    if (href := link["href"]) != "":
+                        property_url: propertyURL = {
+                            "property_id": href.split("/")[2],
+                            "property_url": href,
+                        }
+                        property_links.append(property_url)
                     else:
                         page = False
-           
-                params['index'] += 24
 
+                params["index"] += 24
 
-        print(len(property_links))
-
-
-
-
-    
+        warehouse.load_data("raw.property_links", property_links)
