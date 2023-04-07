e58ab3f7f3e5e80d6375ee64f6943c8da5fa9dd2
zeynepbicer <mericzeynepbicer@gmail.com>
2023-04-07 15:43:59 +0100

    Changes

--- python/extract_property_details.py
+++ python/extract_property_details.py
@@ -30,28 +30,60 @@ template = {
 
 @task(sources="raw.property_links", outputs="raw.property_details")
 def extract_property_details(context: Task, warehouse: Database):
-    properties = warehouse.read_data(
-        query="SELECT * FROM rightmove_raw.property_links WHERE date_added = MAX(date_added)"
-    )
+    """Extract property details from rightmove
+
+    Args:
+        context (Task): Task context
+        warehouse (Database): Database connection
+
+    """
+
+    query = f"""
+        SELECT * FROM {context.src("raw.property_links")}
+        WHERE date_added = (SELECT MAX(date_added) FROM {context.src("raw.property_links")})
+    """
+
+    properties = warehouse.read_data(query=query)
 
     with context.step("Get Propery Details"):
         properties_details = []
-        for _property in properties:
-            property_url = _property["property_url"]
+        for property in properties:
+            if not isinstance(property, dict):
+                raise Exception("Property not a dict")
+            property_url = property["property_url"]
             property_info = template.copy()
-            property_info["property_id"] = _property["property_id"]
+            property_info["property_id"] = property["property_id"]
             property_info["property_url"] = BASE_URL[:-1] + property_url
-            property_info["location_name"] = _property["location_name"]
-            with httpx.Client(base_url=BASE_URL) as client:
-                property_response = client.get(property_url)
-                property_soup = BeautifulSoup(property_response.text, "html.parser")
-            rent_pcm = (
-                property_soup.select_one("div._1gfnqJ3Vtd1z40MlC0MzXu span")
-                .text.replace("£", "")
-                .replace("pcm", "")
-                .replace(",", "")
-                .strip()
-            )
+            property_info["location_name"] = property["location_name"]
+            try:
+                with httpx.Client(base_url=BASE_URL) as client:
+                    property_response = client.get(property_url)
+                    property_soup = BeautifulSoup(property_response.text, "html.parser")
+
+                rent_pcm = (
+                    property_soup.select_one("div._1gfnqJ3Vtd1z40MlC0MzXu span")
+                    .text.replace("£", "")
+                    .replace("pcm", "")
+                    .replace(",", "")
+                    .strip()
+                )
+            except Exception as e:
+                context.log.error(f"Error scraping property {property_url}")
+                context.log.error(f"Error was: {e}")
+
+            if rent_pcm is None:
+                context.log.error(f"No rent found for property {property_url}")
+                continue
+            if not rent_pcm.isdigit():
+                context.log.error(f"Found non-numeric rent for property {property_url}")
+                continue
+
+            yield {
+                "property_url": property_url,
+                "rent_pcm": int(rent_pcm),
+                "date": datetime.datetime.now(),
+            }
+
             property_info["rent_pcm"] = int(rent_pcm)
             property_detail = property_soup.select("dl._2E1qBJkWUYMJYHfYJzUb_r div")
             for dl in property_detail:
@@ -71,14 +103,29 @@ def extract_property_details(context: Task, warehouse: Database):
                 epc_rating["href"] if epc_rating else None
             )
             property_info["deposit"] = property_info["deposit"].replace(",", "")
-            property_info["bedrooms"] = property_info["bedrooms"].replace("x", "")
-            property_info["bathrooms"] = property_info["bathrooms"].replace("x", "")
-            # print(property_info)
-            if property_info["let_available_date"] not in ("Now", "Ask agent"):
+            # replace Ask agent with None and turn it into an integer
+            if property_info["deposit"] == "Ask agent":
+                property_info["deposit"] = None  # type: ignore
+
+            else:
+                property_info["deposit"] = int(property_info["deposit"])
+
+            if property_info["min_tenancy"] == "Ask agent":
+                property_info["min_tenancy"] = None
+
+            property_info["bedrooms"] = property_info["bedrooms"].replace("U+0078", "")
+            property_info["bathrooms"] = property_info["bathrooms"].replace(
+                "U+0078", ""
+            )
+            property_info["size"] = property_info["size"].replace(" sq ft", "")
+            # append to properties details when let_available_date is Now
+            if property_info["let_available_date"] in ("Now", "Ask agent"):
+                if property_info["let_available_date"] == "Now":
+                    properties_details.append(property_info)
+            else:
                 if (
-                    datetime.strptime(
-                        property_info["let_available_date"], "%d/%m/%Y"
-                    ).month
+                    # month number of let available date is greater than 5
+                    int(property_info["let_available_date"].split("-")[1])
                     > 5
                 ):
                     properties_details.append(property_info)
