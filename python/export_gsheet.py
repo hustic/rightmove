81ae217b633e21755219b86f6e68d8b7ce53eabf
zeynepbicer <mericzeynepbicer@gmail.com>
2023-04-07 15:49:51 +0100

    Remove unused imports

--- python/export_gsheet.py
+++ python/export_gsheet.py
@@ -1,13 +1,8 @@
-from itertools import groupby
-from typing import Any, Mapping, Union
+from typing import Mapping
 from google.oauth2 import service_account
-import google.auth
-from google.cloud import bigquery
-from googleapiclient.discovery import build
 from sayn import task
 from sayn.tasks.task import Task
 from sayn.database import Database
-import datetime
 import pygsheets as gs
 import pandas as pd
 
@@ -20,10 +15,6 @@ def extract_gsheet(
 ):
     with context.step("Config"):
         service_account_info = gsheets["service_account"]
-        credentials, _ = google.auth.default(
-            scopes=["https://www.googleapis.com/auth/spreadsheets"]
-        )
-        gsheets_api = build("sheets", "v4", credentials=credentials)
 
     with context.step("Get data"):
         src_table = context.src("intermediate.property_details")
@@ -33,15 +24,14 @@ def extract_gsheet(
     with context.step("Get Gsheets data"):
         # API connection
         # Auth
-        
+
         gsheet_credentials = service_account.Credentials.from_service_account_info(
             service_account_info,
             scopes=("https://www.googleapis.com/auth/spreadsheets",),
         )
         gc = gs.authorize(custom_credentials=gsheet_credentials)
-        sh = gc.open_by_key('15lFBaIpoPkGeWjp3nHhxdKt11gqfI2xqN3v42DCtmFA')
+        sh = gc.open_by_key("15lFBaIpoPkGeWjp3nHhxdKt11gqfI2xqN3v42DCtmFA")
 
         wks = sh[1]
 
         wks.set_dataframe(df, (1, 1))
-   
